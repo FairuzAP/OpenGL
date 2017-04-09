e534a340140538b5e82390faba673abf179fa17d
13514104 <13514104@std.stei.itb.ac.id>
2017-04-09 16:40:46 +0700

    Added Head point_list

--- Glumpy.py
+++ Glumpy.py
@@ -1,6 +1,7 @@
 from glumpy import app, gloo, gl
+from math import cos, sin, radians
+import triangle
 import numpy as np
-from math import *
 
 vertex = """
   
@@ -29,12 +30,18 @@ def transMatrix(scale, dx, dy, rot):
 	return mTrans * mRotat * mScale
 
 # Build the program and corresponding buffers (with 4 vertices)
-left_wing = gloo.Program(vertex, fragment, count=4)
+head = gloo.Program(vertex, fragment, count=29)
 
 # Upload data into GPU
-left_wing['v_color'] = (1,1,1,1)
-left_wing['position'] = [ (+1,0),   (0,-1),   (-1,0),   (0,+1) ]
-left_wing['u_trans'] = transMatrix(1,0,0,45)
+list = [[-1.015,36.871], [3.979,37.855], [5.539,34.606], [10.711,31.199], [12.241,30.883], [13.337,32.007], [11.813,33.732], [9.101,39.207], [8.528,42.404], [8.809,45.242],
+		[9.445,48.649], [7.747,53.191], [8.141,55.137], [8.268,57.48], [7.611,56.837], [7.561,49.884], [8.467,47.643], [7.784,46.787], [6.189,43.444], [5.284,46.047],
+		[5.539,49.162], [3.437,50.96], [0.068,57.598], [-0.959,58.65], [-0.176,55.794], [0.686,53.017], [3.46,48.382], [3.07,43.963], [-5.286,41.142]]
+triangles = triangle.triangulate({'vertices' : list})
+print(triangles)
+
+head['v_color'] = (1,1,1,1)
+head['position'] = list
+head['u_trans'] = transMatrix(1/17,0,-45,0)
 
 # Create a window with a valid GL context
 window = app.Window()
@@ -43,7 +50,7 @@ window = app.Window()
 @window.event
 def on_draw(dt):
     window.clear()
-    left_wing.draw(gl.GL_LINE_LOOP)
+    head.draw(gl.GL_LINE_LOOP)
 
 # Run the app
 app.run()
